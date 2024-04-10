aa54cd3facae9e9d9691d8c8e377bab7915ceab5
skakwy <nils_0805@icloud.com>
2024-04-10 17:14:09 +0200

    maaaa

--- ReRoute/resources/views/welcome.blade.php
+++ ReRoute/resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ReRoute</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    Access-Control-Allow-Origin: *
 </head>
 
 <body>
